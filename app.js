3ce4814c9c5fe065c7b9fe79014c395c32fbf70d
npkane75 <neeraj.p.kane@gmail.com>
2021-07-21 11:55:01 -0700

    start function is working

--- app.js
+++ app.js
@@ -1,55 +1,87 @@
-// setInterval() - Have a function run every set amount of time
-// Call the provided function every 1000 milliseconds
+// // setInterval() - Have a function run every set amount of time
+// // Call the provided function every 1000 milliseconds
+
+// // let count = 0;
+
+// // // Call the provided function every 1000 millisecods
+// // // Increase the count and then log it out every 1 second
+// // let interval = setInterval(function() {
+// //   count++; // increase the count by 1
+
+// //   console.log(count);
+// // }, 1000);
+
+// // // clearInterval will stop the interval that we pass to it
+// // clearInterval(interval);
+
+// ////////////////////////////////////////////////////
+// const startEl = document.getElementById('playbtn');
+// const pauseEl = document.getElementById('lightsbtn');
+// const resetEl = document.getElementById('playbtn');
+// const countEl = document.getElementById('count');
 
 // let count = 0;
+// let interval = null;
 
-// // Call the provided function every 1000 millisecods
-// // Increase the count and then log it out every 1 second
-// let interval = setInterval(function() {
-//   count++; // increase the count by 1
+// function handleStartClick() {
+// interval = setInterval(function() {
+//     // increase the count by one
+//     count++;
 
-//   console.log(count);
+//     // display the new count on the page
+//     countEl.textContent = 'Count: ' + count;
 // }, 1000);
+// }
 
-// // clearInterval will stop the interval that we pass to it
+// function handlePauseClick() {
+//   // Stop the provided interval from running
 // clearInterval(interval);
+// }
 
-////////////////////////////////////////////////////
-const startEl = document.getElementById('btn-start');
-const pauseEl = document.getElementById('btn-pause');
-const resetEl = document.getElementById('btn-reset');
-const countEl = document.getElementById('count');
+// function handleResetClick() {
+//   // Set count back to 0
+// count = 0;
 
-let count = 0;
-let interval = null;
+// countEl.textContent = 'Count: ' + count;
 
-function handleStartClick() {
-interval = setInterval(function() {
-    // increase the count by one
-    count++;
+//   // Stop the interval from running
+// clearInterval(interval);
+// }
 
-    // display the new count on the page
-    countEl.textContent = 'Count: ' + count;
-}, 1000);
+// // When the start button is clicked, call the provided
+// // function
+// startEl.addEventListener('click', handleStartClick);
+// pauseEl.addEventListener('click', handlePauseClick);
+// resetEl.addEventListener('click', handleResetClick); 
+
+//set up class
+
+class Pet {
+    //below is the instance of the class you are creating
+    constructor(name) {
+console.log ('new pet class ready');
+this.name = name;
+this.hunger = 0;
+this.sleep = 0;
+this.boredom = 0;
+    }
 }
 
-function handlePauseClick() {
-  // Stop the provided interval from running
-clearInterval(interval);
+const feedEl = document.getElementById('feedbtn');
+const playEl = document.getElementById('playbtn');
+const sleepEl = document.getElementById('sleepbtn');
+const startEl= document.getElementById('startbtn');
+
+
+//this will be start game function which will take user input and will put that input into the new instance. Specifically into name variable. Then you can call call the function using the button which will be an event.
+
+function startGame() {
+    //the user input is being put into const name variable.
+    const name = prompt('What is your pets name')
+    const myPet = new Pet(name);
+    //this is creating a new class? or renameing the class with user given input 'name'
 }
 
-function handleResetClick() {
-  // Set count back to 0
-count = 0;
-
-countEl.textContent = 'Count: ' + count;
-
-  // Stop the interval from running
-clearInterval(interval);
-}
-
-// When the start button is clicked, call the provided
-// function
-startEl.addEventListener('click', handleStartClick);
-pauseEl.addEventListener('click', handlePauseClick);
-resetEl.addEventListener('click', handleResetClick); 
+startEl.addEventListener('click', startGame);
+// pauseEl.addEventListener('click', handlePauseClick);
+// resetEl.addEventListener('click', handleResetClick); 
